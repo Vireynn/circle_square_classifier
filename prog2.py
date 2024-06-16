2eaf62eb1cdb992356203baba44070348014fd4b
Maxim Dvoretsky <mx.dvoretsky@gmail.com>
2024-06-16 21:49:24 +0300

    Refactor prog2.py, delete some files

--- prog2.py
+++ prog2.py
@@ -14,10 +14,13 @@ with open('config.yaml', 'r') as f_obj:
 data_filename = config['coords_filepath']
 data_c_path = config['circle.path']['circle_data_path']
 data_s_path = config['square.path']['square_data_path']
+c_test_img = config['test.img.path']['circle_test_image']
+s_test_img = config['test.img.path']['square_test_image']
 
 # Logging settings
 logging.basicConfig(level=logging.INFO, filename="prog2_log.log", filemode='w',
                     format="%(asctime)s %(levelname)s %(message)s")
+
 
 def file_load(path: str | os.PathLike):
     try:
@@ -35,6 +38,7 @@ def prob_list(list_, result):
         prob_l.append(elem[result[index]])
     return prob_l
 
+
 # Vector load
 coords = file_load(data_filename)
 
@@ -48,8 +52,8 @@ s_prob_table = file_load(data_s_path)
 
 print(f"Probability table of Square class:\n{s_prob_table}\n")
 
-# ====== Тестовое изображение ======
-img = cv2.imread('img/test_square.png', cv2.IMREAD_GRAYSCALE)
+# Test image
+img = cv2.imread(s_test_img, cv2.IMREAD_GRAYSCALE)  # cv2.imread(c_test_img, cv2.IMREAD_GRAYSCALE)
 test_fern = Figure.bin_attr_fern(img, coords)
 test_split = Figure.fern_split(test_fern, 3)
 res = list(map(lambda x: int(x, 2), test_split))
